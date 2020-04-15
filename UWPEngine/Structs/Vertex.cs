ecbca6fb3c2e39416886e93d63e1703fa825ac5e
Georgi <45877509+GeorgeVelikov@users.noreply.github.com>
2020-04-15 16:46:11 +0100

    Add Vertex struct

---
+++ UWPEngine/Structs/Vertex.cs
@@ -0,0 +1,9 @@
+﻿using SharpDX;
+
+namespace UWPEngine.Structs {
+    public struct Vertex {
+        public Vector3 Normal;
+        public Vector3 Coordinates;
+        public Vector3 WorldCoordinates;
+    }
+}
